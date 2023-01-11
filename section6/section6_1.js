c8d9b1341ed93a69e065ae15fff1427a592a5d7a
Taisei <apple@tai0000.local>
2023-01-11 17:10:26 +0900

    firstcommnit

---
+++ section6/section6_1.js
@@ -0,0 +1,11 @@
+let randomNum;
+
+while (randomNum !== 1) {
+  randomNum = Math.floor(Math.random * 10);
+  console.log(randomNum);
+}
+
+const fruit = [lemon, banaba, apple];
+for (i = 0; i < fruit.length; i++) {
+  console.log(fruit[i]);
+}
